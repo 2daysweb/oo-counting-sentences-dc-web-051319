36e5b40f9083abc11ece39151d81295841826288
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-18 20:31:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -30,7 +30,7 @@ class String
     count = 0
     if (self.split & [".", "?", "!"]).any?
       self.split.each do |sentence|
-       (sentence[-1] & [".", "?", "!"]).any? 
+       ([".", "?", "!"].include?(sentence[-1])
         count+=1
     else
       count
